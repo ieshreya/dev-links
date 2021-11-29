4c45abae34c8115532f8f2342794f272bf466e57
Shreya Purohit <shreya.purohit002@gmail.com>
2021-11-29 23:41:49 +0530

    add animations to links and icons

--- src/components/Links.js
+++ src/components/Links.js
@@ -1,34 +1,39 @@
 function Links() {
 	return (
-		<div className="links">
-			<h4>
-				<span role="img" aria-label="page">
-					📑
-				</span>
-				Get my free ebook
-			</h4>
-			<h4>
-				<span role="img" aria-label="write">
-					✍🏼
-				</span>
-				Visit my blog
-			</h4>
-			<h4>
-				<span role="img" aria-label="star">
-					⭐
-				</span>
-				My Cheatsheets
-			</h4>
-			<h4>
-				<span role="img" aria-label="thread"></span>
-				Web Dev Threads
-			</h4>
-			<h4>
-				<span role="img" aria-label="coffee">
-					☕
-				</span>
-				Support Me
-			</h4>
+		<div className="link-box">
+			<div className="links">
+				<h4>
+					<span role="img" aria-label="page">
+						📑
+					</span>
+					Get my free ebook
+				</h4>
+
+				<h4>
+					<span role="img" aria-label="write">
+						✍🏼
+					</span>
+					Visit my blog
+				</h4>
+				<h4>
+					<span role="img" aria-label="star">
+						⭐
+					</span>
+					My Cheatsheets
+				</h4>
+				<h4>
+					<span role="img" aria-label="thread">
+						🧵
+					</span>
+					Web Dev Threads
+				</h4>
+				<h4>
+					<span role="img" aria-label="coffee">
+						☕
+					</span>
+					Support Me
+				</h4>
+			</div>
 		</div>
 	);
 }
